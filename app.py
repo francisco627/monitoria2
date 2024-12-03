74a0502abce7bf7d3a8cb87eec9b8bec165a3bdd
francisco627 <ffaguinho77@gmail.com>
2024-12-03 12:33:08 -0300

    aj

--- app.py
+++ app.py
@@ -550,4 +550,4 @@ def download_file(filename):
 
 
 if __name__ == "__main__":
-    serve(app, host='0.0.0.0', port=8080)  # Usando waitress 
+    serve(app, host='127.0.0.1', port=8000)  # Usando waitress 
